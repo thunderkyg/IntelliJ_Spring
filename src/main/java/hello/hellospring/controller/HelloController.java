2b9285b745b7d011c9301549376a6c88a7f2397e
Thomas Kim <thunderkyg@gmail.com>
2024-01-31 23:54:08 +0900

    ModelandView 방식으로 출력

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,14 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
-    public String hello(Model model) {
-        model.addAttribute("date", "hello!!");
-        return "hello";
+    public ModelAndView hello() {
+
+        return new ModelAndView("index.html");
     }
 }

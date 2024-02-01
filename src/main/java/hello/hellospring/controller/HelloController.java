4144faf890ba1ac27b923cd20236229ffc95a3fd
Thomas Kim <thunderkyg@gmail.com>
2024-02-01 22:22:41 +0900

    강의 업데이트 방식으로 출력

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,6 +1,7 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -8,8 +9,17 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 
     @GetMapping("hello")
-    public ModelAndView hello() {
+    public String hello(Model model) {
+        model.addAttribute("date", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("modelandview")
+    public ModelAndView hello(HttpServletRequest req, HttpServletResponse res) {
 
         return new ModelAndView("index.html");
     }
+
+
+
 }

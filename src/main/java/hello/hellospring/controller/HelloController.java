d94bcaec79f57badfb7cdfc35b328fccae33dfb7
Thomas Kim <thunderkyg@gmail.com>
2024-02-21 13:03:00 +0900

    Push Pull 테스트 커밋

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 @Controller
 public class HelloController {
 
+    //PUSH/PULL TESTING
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("date", "hello!!");
